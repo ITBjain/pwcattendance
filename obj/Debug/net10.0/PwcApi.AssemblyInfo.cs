3d452f649d4b0ec107326bbed19a9ca59bd36ac9
Haceron <pandeypriyanshu2343@gmail.com>
2026-05-19 10:31:01 +0530

    Migrating to live db

--- obj/Debug/net10.0/PwcApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/PwcApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PwcApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a6ce76648c1aecef879c91e10f875558509768d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94010a0414cf2df3c585654e26d773480e8b8678")]
 [assembly: System.Reflection.AssemblyProductAttribute("PwcApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PwcApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
