e3edb21d4d50807f075dd430db792dae438b01fa
Haceron <pandeypriyanshu2343@gmail.com>
2026-05-28 16:33:26 +0530

    live db

--- obj/Debug/net10.0/PwcApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/PwcApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PwcApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c894fafe6ac6c5a1bb8eda48c0026c662b0ecac8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fbae29d3506ba30cbae3b3f062bcf166c85b11d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PwcApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PwcApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
